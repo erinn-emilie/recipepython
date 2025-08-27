2cc81cb043fb3744885689b3b9b5084889d6390e
erinn-emilie <erinnemiliek@gmail.com>
2025-08-27 11:45:37 -0400

    Made optimizations and changes and implemented error catching in recipepython.

--- recipepython.py
+++ recipepython.py
@@ -41,10 +41,12 @@ loginCodes = {
 }
 
 saveRecipeCodes = {
-    "NOMESSAGE": 0,
-    "INVALUSER": 1,
-    "DOUBLESAVE": 2,
-    "SUCCESS": 3
+    "FAILURE": 0,
+    "SUCCESS": 1
+}
+
+badParseCodes = {
+    "INVALURL": 0
 }
 
 
@@ -78,121 +80,110 @@ class Recipes(database.Model):
     url: Mapped[str]
 
 
+def check_key(dictionary, key):
+    if key in dictionary:
+        return dictionary[key]
+    return ""
+
+def check_keys(dictionary, key1, key2):
+    if key1 in dictionary:
+        if key2 in dictionary[key1]:
+            return dictionary[key1][key2]
+    return ""
+
+def find_right_key(dictionary, keyList):
+    for key in keyList:
+        if key in dictionary:
+            return dictionary[key]
+    return ""
+
+def find_right_dict_and_key(dictionary, primKey, keyList):
+    if primKey in dictionary:
+        find_right_key(dictionary[primKey], keyList)
+    else:
+        return ""
+
+
+
+
 
 def parse_allrecipes(json_data):
     json_dict = json_data[0]
 
-    stepsList = json_dict["recipeInstructions"]
-    instructions = []
+    stepsList = check_key(json_dict,"recipeInstructions")
+    ingredients = check_key(json_dict,"recipeIngredient")
+    if stepsList != "" and ingredients != "":
+        instructions = []
 
-    for step in stepsList:
-        instructions.append(step["text"])
+        for step in stepsList:
+            instructions.append(step["text"])
 
-
-    if "aggregateRating" in json_dict:
-        rating = json_dict["aggregateRating"]["ratingValue"]
-        reviews = json_dict["aggregateRating"]["ratingCount"]
+        return_dictionary = {
+            "author": json_dict["author"][0]["name"],
+            "name": check_key(json_dict,"headline"),
+            "cuisine": check_key(json_dict,"recipeCuisine"),
+            "category": check_key(json_dict,"recipeCategory"),
+            "ingredients": ingredients,
+            "cook_time": check_key(json_dict,"cookTime"),
+            "rating": check_keys(json_dict,"aggregateRating", "ratingValue"),
+            "reviews": check_keys(json_dict,"aggregateRating","ratingCount"),
+            "yield": check_key(json_dict,"recipeYield"),
+            "instructions": instructions,
+            "date": check_key(json_dict,"datePublished"),
+            "site_name": check_keys(json_dict,"publisher","name"),
+            "nutrition": check_key(json_dict,"nutrition"),
+            "userliked": "false"
+        }
     else:
-        rating = "Not available"
-        reviews = "Not available"
-
-    return_dictionary = {
-        "author": json_dict["author"][0]["name"],
-        "name": json_dict["headline"],
-        "cuisine": json_dict["recipeCuisine"],
-        "category": json_dict["recipeCategory"],
-        "ingredients": json_dict["recipeIngredient"],
-        "cook_time": json_dict["cookTime"],
-        "rating": rating,
-        "reviews": reviews,
-        "yield": json_dict["recipeYield"],
-        "instructions": instructions,
-        "date": json_dict["datePublished"],
-        "site_name": json_dict["publisher"]["name"],
-        "nutrition": json_dict["nutrition"],
-        "userliked": "false"
-    }
+        return_dictionary = {"message": "FAILURE"}
 
     return return_dictionary
 
 
-def parse_other(json_data, urlType):
+def parse_other(json_data):
 
     json_dict = json_data["@graph"]
-
+    return_dictionary = {}
+    site_name = ""
 
     for dic in json_dict:
         if dic["@type"] == "Organization" or dic["@type"] == "WebSite":
-            site_name = dic["name"]
+            site_name = check_key(dic,"name")
 
         if dic["@type"] == "Recipe":
 
-            if "@id" in dic["author"]:
-                author = dic["author"]["@id"]
-            elif "name" in dic["author"]:
-                author = dic["author"]["name"]
+            recipe_yield = check_key(dic,"recipeYield")
+            if isinstance(recipe_yield,list):
+                recipe_yield = recipe_yield[0]
+
+            ingredients = check_key(dic,"recipeIngredient")
+            stepsList = check_key(dic,"recipeInstructions")
+
+            if ingredients != "" and stepsList != "":
+                stepsList = dic["recipeInstructions"]
+                instructions = []
+                for step in stepsList:
+                    if isinstance(step, dict) and check_key(step, "text") != "":
+                        instructions.append(step["text"])
+                               
+                return_dictionary = {
+                    "author": find_right_key(dic["author"], ["@id", "name"]),
+                    "name": check_key(dic,"name"),
+                    "cuisine": check_key(dic,"recipeCuisine"),
+                    "category": check_key(dic,"recipeCategory"),
+                    "ingredients": ingredients,
+                    "cook_time": check_key(dic,"cookTime"),
+                    "rating": find_right_dict_and_key(dic,"aggregateRating", ["ratingValue"]),
+                    "reviews": find_right_dict_and_key(dic, "aggregateRating",["ratingCount","reviewCount"]),
+                    "yield": recipe_yield,
+                    "instructions": instructions,
+                    "date": check_key(dic,"datePublished"),
+                    "site_name": site_name,
+                    "nutrition": check_key(dic,"nutrition"),
+                    "userliked": "false"
+                }
             else:
-                author = "Not available"
-
-
-            if "aggregateRating" in dic:
-                rating = dic["aggregateRating"]["ratingValue"]
-                if "ratingCount" in dic["aggregateRating"]:
-                    review_count =  dic["aggregateRating"]["ratingCount"]
-                elif "reviewCount" in dic["aggregateRating"]:
-                    review_count = dic["aggregateRating"]["reviewCount"]
-                else:
-                    review_count = "Not available"
-            else:
-                rating = "Not available"
-                review_count = "Not available"
-
-
-            if "nutrition" in dic:
-                nutrition = dic["nutrition"]
-            else:
-                nutrition = {}
-
-
-
-            if urlType == "modernhoney":
-                recipe_yield = dic["recipeYield"]
-
-            else:
-                recipe_yield = dic["recipeYield"][1]
-
-            recipe_name = dic["name"]
-            cuisine_type = dic["recipeCuisine"]
-            recipe_category = dic["recipeCategory"]
-            ingredients = dic["recipeIngredient"]
-            cook_time = dic["cookTime"]
-            date_published = dic["datePublished"]
-
-
-
-            stepsList = dic["recipeInstructions"]
-            instructions = []
-
-            for step in stepsList:
-                instructions.append(step["text"])
-
-    
-    return_dictionary = {
-        "author": author,
-        "name": recipe_name,
-        "cuisine": cuisine_type,
-        "category": recipe_category,
-        "ingredients": ingredients,
-        "cook_time": cook_time,
-        "rating": rating,
-        "reviews": review_count,
-        "yield": recipe_yield,
-        "instructions": instructions,
-        "date": date_published,
-        "site_name": site_name,
-        "nutrition": nutrition,
-        "userliked": "false"
-    }
+                return_dictionary = {"message": "FAILURE"}
 
     return return_dictionary
 
@@ -262,8 +253,9 @@ def save_to_database(data_dict, url):
 
     nutrition = data_dict["nutrition"]
     nutritionStr = ""
-    for key, value in nutrition.items():
-        nutritionStr = nutritionStr + "---" + str(key) +":" + str(value)
+    if isinstance(nutrition, dict):
+        for key, value in nutrition.items():
+            nutritionStr = nutritionStr + "---" + str(key) +":" + str(value)
 
     newrecipe = Recipes(
         author = data_dict["author"],
@@ -308,28 +300,40 @@ def run_script():
                     savedInt = int(saved)
                     if savedInt == recipeID:
                         return_dictionary["userLiked"] = "true"
+
+        return return_dictionary
+
     else: 
         # If it isn't we parse it from the website
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'}
-        page = requests.get(url, headers=headers)
-        soup = BeautifulSoup(page.text, 'html.parser')
+        try:
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'}
+            page = requests.get(url, headers=headers)
+            page.raise_for_status()  
 
-        json_script_tag = soup.find('script', type='application/ld+json')
-        json_string = json_script_tag.string
-        json_data = json.loads(json_string)
+            soup = BeautifulSoup(page.text, 'html.parser')
 
-        if "allrecipes" in url:
-            return_dictionary = parse_allrecipes(json_data)
-        elif "modernhoney" in url:
-            return_dictionary = parse_other(json_data, "modernhoney")
-        else:
-            return_dictionary = parse_other(json_data, "pinchofyum")
+            json_script_tag = soup.find('script', type='application/ld+json')
+            json_string = json_script_tag.string
+            json_data = json.loads(json_string)
 
-        return_dictionary["name"] = return_dictionary["name"].replace("&#39;", "'")
+            if "allrecipes" in url:
+                return_dictionary = parse_allrecipes(json_data)
+            else:
+                return_dictionary = parse_other(json_data)
 
-        save_to_database(return_dictionary, url)
+            message = check_key(return_dictionary, "message")
+            if message == "":
+                return_dictionary["name"] = return_dictionary["name"].replace("&#39;", "'")
+                save_to_database(return_dictionary, url)
+        
+            return return_dictionary
 
-    return return_dictionary
+        except requests.exceptions.MissingSchema as e:
+            return {"message": badParseCodes["INVALURL"]}
+        except requests.exceptions.HTTPError as e:
+            return {"message": badParseCodes["INVALURL"]}
+
+
 
 
 
@@ -337,9 +341,6 @@ def run_script():
 def login_script():
     username = request.json["username"]
     password = request.json["password"].encode('utf-8') 
-
-    
-
 
     user = database.session.scalars(database.select(Users).filter_by(username=username)).one_or_none()
     message = loginCodes["NOMESSAGE"]
@@ -355,7 +356,6 @@ def login_script():
             userid = user.userID
         else:
             message = loginCodes["INVALINFO"]
-
 
     return {"message": message, "userid": userid}
 
@@ -416,45 +416,35 @@ def save_recipe():
     # Initalizing some helper variables, message will be returned to client, initially there is no message
     # doublesave indicated that the user has already saved this recipe, initially it is false
     # finalsaverecipes is a placeholder for the string that will eventually hold all the recipe id's that the user has saved
-    message = saveRecipeCodes["NOMESSAGE"]
-    doublesave = False
+    message = saveRecipeCodes["FAILURE"]
     finalsaverecipes = ""
 
     # user and recipe are the database objects that represent the relevent user and recipe
     user = database.session.scalars(database.select(Users).filter_by(userID=userid, username=username)).one_or_none()
     recipe = database.session.scalars(database.select(Recipes).filter_by(url=url, name=recipename)).one_or_none()
 
-    # savedrecipes is the string that holds the users' current saved recipes string
-    savedrecipes = user.savedrecipes
+    if not recipe is None and user is not None:
+        # savedrecipes is the string that holds the users' current saved recipes string
+        savedrecipes = user.savedrecipes
 
-    # recipeid is set to the unique id of the recipe
-    recipeid = recipe.recipeID
+        # recipeid is set to the unique id of the recipe
+        recipeid = recipe.recipeID
 
-    # If the user already has saved recipes
-    if not savedrecipes is None:
-        # We split up the recipe ids' in the string and iterate through them, where curID is the current id from the string that we are on
-        slices = savedrecipes.split("--")
-        for bite in slices:
-            bite = bite.strip()
-            curID = int(bite)
-            # If curID is equal to the recipeid we are looking for, that means the user has already saved this recipe. The message is set and doublesave is set to true
-            if curID == recipeid:
-                message = saveRecipeCodes["DOUBLESAVE"]
-                doublesave = True
-                break
-        # If doublesave isn't true, we append the recipeid to the end of the users' current savedrecipes string and store it in finalsaverecipes. The message is success
-        if not doublesave:
-            finalsaverecipes = savedrecipes + "--" + str(recipeid)
+        # If the user already has saved recipes
+        if not savedrecipes is None:
+            finalsaverecipes = savedrecipes + "---" + str(recipeid)
             message = saveRecipeCodes["SUCCESS"]
-    # If savedrecipes is empty, finalsaverecipes becomes just this recipeid, as it is the only recipe the user has saved.
-    else:
-        finalsaverecipes = str(recipeid)
-        message = saveRecipeCodes["SUCCESS"]
+        # If savedrecipes is empty, finalsaverecipes becomes just this recipeid, as it is the only recipe the user has saved.
+        else:
+            finalsaverecipes = str(recipeid)
+            message = saveRecipeCodes["SUCCESS"]
 
-    # If we have a message of succes we can go ahead and change the users' savedrecipes string to our finalsaverecipes and commit that to the database
-    if message == saveRecipeCodes["SUCCESS"]:
-        user.savedrecipes = finalsaverecipes
-        database.session.commit()
+        # If we have a message of succes we can go ahead and change the users' savedrecipes string to our finalsaverecipes and commit that to the database
+        if message == saveRecipeCodes["SUCCESS"]:
+            user.savedrecipes = finalsaverecipes
+            database.session.commit()
+    else:
+        message = saveRecipeCodes["FAILURE"]
 
     # Then we return the message
     return {"message": message}
